f522fb876e02f0da070e7c6e6dbe1e724641c3ff
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-02-15 17:50:27 +0800

    Added loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Loader from '../components/Loader'
+
 export default function Home() {
   return (
     <div >
-      
+      <h1>Hello</h1>
+      <Loader show/>
     </div>
   )
 }
