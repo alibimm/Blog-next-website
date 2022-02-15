f1c353ef8d74008dbe5da3668b54eb4eb989bd90
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-02-15 16:37:11 +0800

    initialised firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6j8HprfPrfdUDTm8hHnN-jtf76i1e5CM",
+  authDomain: "blog-website-580fd.firebaseapp.com",
+  projectId: "blog-website-580fd",
+  storageBucket: "blog-website-580fd.appspot.com",
+  messagingSenderId: "690662911458",
+  appId: "1:690662911458:web:dc9c56b9936e56e6291dbb",
+  measurementId: "G-VZGWGM5XTC"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
