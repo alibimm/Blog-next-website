6826515824ee9d8df39f6129b2b4076f853677bf
Alibi Melis <melis.alibi2000@gmail.com>
2022-03-14 23:26:33 +0600

    Increased the posts load limit to 5

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 import { useState } from "react";
 import PostFeed from "../components/PostFeed";
 
-const LIMIT = 1;
+const LIMIT = 5;
 
 export async function getServerSideProps(context) {
   let ref = firestore
